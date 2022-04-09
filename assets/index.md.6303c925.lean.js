cc6d87194455ac7b3246263aa606a3499ffa474e
tbosmans <tom.94.bosmans@gmail.com>
2022-04-09 18:32:21 +0200

    deploy

---
+++ assets/index.md.6303c925.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.e87f5ba4.js";const m='{"title":"Home","description":"","frontmatter":{"home":true,"heroImage":"/logo.jpg"},"headers":[],"relativePath":"index.md","lastUpdated":1649521773000}',o={};function r(n,s,c,d,p,i){return a(),t("div")}var l=e(o,[["render",r]]);export{m as __pageData,l as default};
